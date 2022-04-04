eba3605b69b3722fbdcfbe07c24e1cdd3a35b59d
Phils Garcia <eivanphils@gmail.com>
2022-04-04 18:49:42 -0400

    agregado variables de tipo strings

--- ejercicios/01_basico/02_datatypes.dart
+++ ejercicios/01_basico/02_datatypes.dart
@@ -4,9 +4,35 @@ main() {
   int a = 10;
   double b = 10.34;
   int? c;
+  int? d;
   
 
   print(a);
   print(b);
   print(c);
+  print(d);
+  
+  
+  // ====== string
+  
+  String name = 'Phils';
+  String? name2;
+  String name3 = "Hola $name";
+  
+  String multilinea = '''
+  
+  Hola mundo
+  como estas?
+  demo dart
+  multilineas
+  
+  name3 = $name3  
+  
+  ''';
+  
+  print(name.compareTo('$name3'));
+  print(name);
+  print(name2);
+  print(name3);
+  print(multilinea);
 }
