6a26bbb773afde6e0f3b5eb4ab4b7bf72e708e61
Phils Garcia <eivanphils@gmail.com>
2022-04-05 19:26:01 -0400

    agregadas variables final, const y late

---
+++ ejercicios/01_basico/03_finalConst.dart
@@ -0,0 +1,20 @@
+main() {
+  
+  var a = 11;
+  
+  final b = 45;
+  
+  const c = 30;
+  
+  print(b);
+  
+  final List<String> personas = ['Phils', 'David'];
+  
+  personas.add('Jesus');
+  
+  List<String> villanos = const['Thanos', 'Cell'];
+    
+  
+  print(personas); 
+  print(villanos);    
+}
