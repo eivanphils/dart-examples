31a75f996b612958c77a155f36327da2349ac675
Phils Garcia <eivanphils@gmail.com>
2022-04-05 19:42:14 -0400

    agregado comentarios en dart

---
+++ ejercicios/01_basico/04_comentarios.dart
@@ -0,0 +1,19 @@
+main() {
+
+  // comentario uno
+  int a = 10; // valor de a 10
+
+  final personas = [
+    //Inicio de comentario
+    'Phis',
+    //entre medio
+    'jose' // posicion 1
+    // fin comentario
+  ];
+
+  print(personas)
+}
+
+/// Funciona para cargar scripts
+/// Recibe un [nombre] y se concatena con el [mensaje] 
+loadScripts(int nombre, int mensaje) {}
