f02f4a7e7c8f256e4d3a507831d2b2d67e9c3339
Phils Garcia <eivanphils@gmail.com>
2022-04-07 19:50:43 -0400

    arreglada variables sin uso

--- ejercicios/01_basico/04_comentarios.dart
+++ ejercicios/01_basico/04_comentarios.dart
@@ -1,5 +1,4 @@
 main() {
-
   // comentario uno
   int a = 10; // valor de a 10
 
@@ -11,9 +10,10 @@ main() {
     // fin comentario
   ];
 
-  print(personas)
+  print(a);
+  print(personas);
 }
 
 /// Funciona para cargar scripts
-/// Recibe un [nombre] y se concatena con el [mensaje] 
+/// Recibe un [nombre] y se concatena con el [mensaje]
 loadScripts(int nombre, int mensaje) {}
--- ejercicios/01_basico/06_operadores.dart
+++ ejercicios/01_basico/06_operadores.dart
@@ -1,35 +1,34 @@
 main() {
-
   // operadores de asignacion
   int a = 10;
-  
+
+  print(a);
   int? b;
-  
+
   b ??= 20; // asignar valor si la variables es nula;
-  
+
   print(b);
-  
+
   b = b != 20 ? 30 : 4;
-  
+
   print(b);
-  
+
   // operador condicional
-  
+
   int c = 0;
-  
+
   String response = c > 25 ? 'C es mayor a 25' : 'C es menor de 25';
-  
+
   print(response);
-  
+
   int? x;
   int? y;
   int z = 123;
-  
+
   int d = x ?? y ?? z;
-  
+
   print(d);
-  
-  
+
   // operadores relaciones
   /*
    * > mayor que
@@ -41,28 +40,27 @@ main() {
    * != si dos objetos son distintos 
    * 
    */
-  
+
   String name = 'jose';
   String name2 = 'Ivan';
-  
+
   print(name == name2);
   print(name != name2);
-  
-  
+
   int first = 123;
   int second = 222;
-  
+
   print(first > second);
   print(first < second);
-  
+
   print(first >= second);
   print(first <= second);
-  
+
   // operador de tipo
   int i = 10;
   String j = '10';
-  
+
+  // ignore: unnecessary_type_check
   print(i is int);
   print(j is! int);
-  
 }
