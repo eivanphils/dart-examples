5217e852fb9e17370fd89ea1c37747aa5702f0a3
Phils Garcia <eivanphils@gmail.com>
2022-04-21 17:12:11 -0400

    agregado propiedades final

---
+++ 05_clases/02_propiedades_final.dart
@@ -0,0 +1,30 @@
+class Cuadrado {
+  final int lado;
+  final int area;
+
+
+  // necesito calcular el area multiplicando lado * lado
+  // Cuadrado(this.lado, this.area);
+
+  // no se puede seter de esta manera ya que las varialbes son de tipo final
+  // Cuadrado(lado, this.area){
+  //   this.lado = lado;
+  // }
+
+  Cuadrado(int lado):
+    this.lado = lado,
+    this.area = lado * lado;
+
+  String printCuadrado() => 'El lado es $lado y su area es $area';
+
+}
+
+void main() {
+
+  final miCuadrado = new Cuadrado(4);
+
+  print(miCuadrado.lado);
+  print(miCuadrado.area);
+  print(miCuadrado.printCuadrado());
+  
+}
