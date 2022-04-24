52c8705bff4529851c09a600b7707cb608b32c16
Phils Garcia <eivanphils@gmail.com>
2022-04-24 16:27:02 -0400

    agregado clases mixins

---
+++ 06_clases_adv/05_mixins.dart
@@ -0,0 +1,46 @@
+// no se pueden crear instancias
+// no tiene constructor
+// solo se puede transferir a otra clase
+import '05_mixins2.dart';
+
+mixin Logger {
+  void imprimir(String texto) {
+    final hoy = new DateTime.now();
+    print('$hoy :::: $texto');
+  }
+}
+
+// puede funcionar como un mixin
+// pero si se agrega el constructor se debe usar extends en vez de with
+class Logger2 {
+  void imprimir2(String texto) {
+    final hoy = new DateTime.now();
+    print('$hoy :::: $texto');
+  }
+}
+
+abstract class Astro with Logger {
+  String nombre = 'as';
+
+  Astro() {
+    imprimir('init astro');
+  }
+
+  void existo() {
+    imprimir('soy un ser celestial');
+  }
+}
+
+class Asteroide extends Astro with Logger, Logger2 {
+  String nombre;
+
+  Asteroide(this.nombre) {
+    imprimir(this.nombre);
+    imprimir2(this.nombre);
+  }
+}
+
+void main() {
+  final thanos = new Asteroide('thanos');
+  thanos.existo();
+}
